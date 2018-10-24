3c8f875f94c24b57c21f51e39338882282769b7b
martin.dobrev@noser.com <LEviZU87>
2018-10-24 16:41:56 +0300

    adding thumbnailator as dependency

--- src/main/java/com/noser/blog/controller/FileController.java
+++ src/main/java/com/noser/blog/controller/FileController.java
@@ -5,6 +5,10 @@ import com.noser.blog.domain.BlogFile;
 import com.noser.blog.mapper.FileMapper;
 import com.noser.blog.service.FileService;
 import lombok.extern.slf4j.Slf4j;
+import net.coobird.thumbnailator.Thumbnailator;
+import net.coobird.thumbnailator.Thumbnails;
+import net.coobird.thumbnailator.makers.ThumbnailMaker;
+import net.coobird.thumbnailator.util.ThumbnailatorUtils;
 import org.apache.http.entity.ContentType;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
@@ -14,9 +18,15 @@ import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
-import javax.websocket.server.PathParam;
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
 import java.security.Principal;
 import java.time.LocalDateTime;
+import java.util.Optional;
 
 @Slf4j
 @RestController
@@ -48,15 +58,28 @@ public class FileController {
   }
 
   @GetMapping(value = "/files/{id}")
-  public @ResponseBody ResponseEntity<byte[]> getFile(@PathVariable Long id) {
+  public @ResponseBody ResponseEntity<byte[]> getFile(@PathVariable Long id, @RequestParam Optional<Integer> size) throws IOException {
     final BlogFile file = this.fileService.getFile(id);
     final HttpHeaders httpHeaders = new HttpHeaders();
+
     if (file == null) {
       return new ResponseEntity<>(null, httpHeaders, HttpStatus.NOT_FOUND);
     }
-    httpHeaders.add("Content-Type", ContentType.getByMimeType(file.getFileType()).toString());
-    ResponseEntity<byte[]> responseEntity = new ResponseEntity<>(file.getData(), httpHeaders, HttpStatus.OK);
-    return responseEntity;
 
+    if (size.isPresent()) {
+      httpHeaders.add("Content-Type", ContentType.IMAGE_JPEG.getMimeType());
+      return new ResponseEntity<>(createThumbnail(file, size.get()), httpHeaders, HttpStatus.OK);
+    }
+
+    httpHeaders.add("Content-Type", ContentType.getByMimeType(file.getFileType()).toString());
+    return new ResponseEntity<>(file.getData(), httpHeaders, HttpStatus.OK);
+  }
+
+  private byte[] createThumbnail(final BlogFile file, int size) throws IOException {
+    InputStream imageIn = new ByteArrayInputStream(file.getData());
+    BufferedImage bufferedImage = Thumbnails.of(imageIn).size(size, size).asBufferedImage();
+    ByteArrayOutputStream baos = new ByteArrayOutputStream();
+    ImageIO.write(bufferedImage, "jpg", baos);
+    return baos.toByteArray();
   }
 }
